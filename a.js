29a8157d698da8c32a9866bcad9ea40ffb2b4845
fanhaorun <fanhaorun@qtrade.com.cn>
2022-08-12 19:09:07 +0800

    Revert "Merge pull request #1 from f641753527/a"
    This reverts commit 05f833e468cc007cb38947982b72e5dee6fcc074, reversing
    changes made to 9b4a2d559426be993dc88762b48897f4816f2977.

--- a.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: Phoenix Fan
- * @Date: 2022-08-12 18:47:36
- * @LastEditors: Phoenix Fan
- * @LastEditTime: 2022-08-12 18:47:36
- * @Description: 
- */
-console.log('a 分支提交的代码并发版')
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: Phoenix Fan
  * @Date: 2022-08-12 18:45:55
  * @LastEditors: Phoenix Fan
- * @LastEditTime: 2022-08-12 19:00:33
+ * @LastEditTime: 2022-08-12 19:08:54
  * @Description: 
  */
 console.log('b分支修改功能代码(在a后面修改)')
